4b778a34a1d51fac426fa003be1016f424be27ce
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2021-12-28 18:56:27 -0500

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream> 
+using namespace std;
+
+//My birthday is on Canada Day! 🍁
+
+int main() {
+	cout << " SSS        A " << endl;
+	cout << "S   S      A A" << endl;
+	cout << "S         A   A" << endl;
+	cout << " SSS      AAAAA" << endl;
+	cout << "    S     A   A" << endl;
+	cout << "S   S     A   A" << endl;
+	cout << " SSS      A   A " << endl;
+
+
+	system("pause");
+	return 0;
+}
